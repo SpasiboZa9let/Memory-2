4a956867ea07d16b7f4df1e3a1db19a267f65f08
Иван <foxneversleep@gmail.com>
2025-07-12 15:21:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Memory-2/', // 👈 если репозиторий называется memory-map
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
 
