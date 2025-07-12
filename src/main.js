ab1eac4a9ed6001a06f2d2220804de76120233c1
Иван <foxneversleep@gmail.com>
2025-07-12 17:41:18 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
+// src/main.js
 
+// Определяем, какой экран открыт
+const page = document.body.dataset.page;
+
+async function init() {
+  try {
+    switch (page) {
+      case 'index':
+        const indexModule = await import('./views/index.js');
+        indexModule.initMap();
+        break;
+      case 'start':
+        const startModule = await import('./views/start.js');
+        startModule.initStart();
+        break;
+      case 'album':
+        const albumModule = await import('./views/album.js');
+        albumModule.initAlbum();
+        break;
+      default:
+        console.warn('main.js: неизвестная страница');
+    }
+  } catch (err) {
+    console.error('main.js: ошибка при инициализации:', err);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
